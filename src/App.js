234f66ffaca72956d01210b588d443f2be46c27b
azadeh-92 <azadeh.fazlolahi70@gmail.com>
2023-08-13 12:35:44 +0330

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Weather App</p>
+      <container>
+        <p>Weather App</p>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/azadeh-92">Azadeh</a> and is{" "}
+          <a href="https://github.com/azadeh-92/react2">
+            open-source on Github
+          </a>
+        </footer>
+      </container>
     </div>
   );
 }
