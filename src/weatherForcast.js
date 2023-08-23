ceb00f488756819580e9cca157e0b42a29d48448
azadeh-92 <azadeh.fazlolahi70@gmail.com>
2023-08-23 10:26:43 +0330

    fix errors

--- src/weatherForcast.js
+++ src/weatherForcast.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./forcast.css";
 import axios from "axios";
-import WeatherIcon from "./weatherIcon";
 import ForcastData from "./forcastData";
 export default function WeatherForcast(props) {
   let [forcast, setForcast] = useState(null);
   let [load, setLoad] = useState(false);
-  let numberOfDays = [1, 2, 3, 4, 5];
+
   function handleResponse(response) {
     console.log(response.data.daily[0]);
     setForcast(response.data.daily);
@@ -26,6 +25,8 @@ export default function WeatherForcast(props) {
                 <ForcastData data={day} />
               </div>
             );
+          } else {
+            return null;
           }
         })}
       </div>
